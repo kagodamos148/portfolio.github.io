0e5e218d276945c2ee8523ee36094d2082f506a0
Rakesh Jinka <jinkarakesh.cs19@rvce.edu.in>
2025-08-21 19:38:33 +0530

    Checkpoint2 : Boiler plate for next js tailwind based project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// Local imports
+import NavBar from "@/components/NavBar";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,9 +27,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        < NavBar />
         {children}
       </body>
     </html>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        celestial: '#70B9F0',
+        emerald: '#1CA75B',
+        ruby: '#C41E3A',
+        amber: '#F2B63D',
+      },
+    },
+  },
+  plugins: [],
+}
